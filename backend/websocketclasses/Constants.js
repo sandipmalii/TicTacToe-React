ea6b5db7c54bee257f1a3006f4735e93d5c9fdbb
pradeep <pradeepubuntuvps@gmail.com>
2024-12-14 17:48:43 +0530

    Add placing moves functionality inside a started game using sockets

--- backend/websocketclasses/Constants.js
+++ backend/websocketclasses/Constants.js
@@ -1,5 +1,9 @@
 export const INIT_GAME = "start_game";
+export const INIT_GAME_RESPONSE = "game_started";
 export const PLACE = "place";
 export const CHAT_MESSAGE = "chat";
 export const GAME_OVER = "game_over";
+export const INVALID = "invalid";
+export const INVALID_TURN = "invalid_turn";
+export const INVALID_INDEX = "invalid_index";
 
--- backend/websocketclasses/GameManager.js
+++ backend/websocketclasses/GameManager.js
@@ -1,5 +1,5 @@
 import Player from "./Player.js";
-import { INIT_GAME } from "./Constants.js";
+import { CHAT_MESSAGE, INIT_GAME, PLACE } from "./Constants.js";
 import Game from "./Game.js";
 
 export class GameManager {
@@ -19,15 +19,31 @@ export class GameManager {
 			if (type === INIT_GAME) {
 				//check if a previous player is waiting and make a match if present
 				player.setName(payload.name);
+				console.log(player.name);
 				if (this.pendingUser) {
 					const newGame = new Game(player, this.pendingUser);
 					this.games.push(newGame);
 					this.pendingUser = null;
-					console.log("created game");
 				}
 				// else make the user wait until other user starts matchmaking
 				else {
 					this.pendingUser = player;
+				}
+			}
+			if (type === PLACE) {
+				const currentGame = this.games.find(
+					(game) => game.player1 == player || game.player2 == player
+				);
+				//If there is no game associated with the player just do nothing.
+				if (!currentGame) {
+					return;
+				}
+				const message = currentGame.placeSymbol(player, payload.index);
+				if (message == "draw" || message == "won") {
+					this.games = this.games.filter(
+						(game) => game.id != currentGame.id
+					);
+					console.log(this.games);
 				}
 			}
 		});
